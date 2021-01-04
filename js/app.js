1c18d9e3b7ca7d72f272e33dee59675bc5bb0887
JamilaOthman <mema20330@gmail.com>
2021-01-04 16:08:19 +0200

    lab2

---
+++ js/app.js
@@ -0,0 +1,47 @@
+
+'use strict'
+
+var Q1='y';
+var Q1=prompt("Am I have bachelors degree?" );
+if(Q1.toLowerCase()==='y'||Q1.toLowerCase==='yes'){
+    alert('good start');
+    // console.log('good start')
+} else{
+    alert('Oopsno');
+}
+
+var Q2='n';
+var Q2=prompt("Do I like cats");
+if(Q2.toLowerCase()==='n' ||Q2.toLowerCase==='no'){
+    alert('you are right');
+    // console.log('you are right')
+} else{
+    alert('are you sure???!!');
+}
+
+var Q3='y';
+var Q3=prompt("Do I live in Amman? ");
+if(Q3.toLowerCase()==='y' ||Q3.toLowerCase==='yes'){
+    alert('yes I do');
+    // console.log('right')
+} else{
+    alert('actually I do');
+}
+
+var Q4='y';
+var Q4=prompt("Do I like cooking?" );
+if(Q4.toLowerCase()==='y'||Q4.toLowerCase==='yes'){
+    alert('Correct!!');
+    // console.log('right')
+} else{
+    alert('actually I do');
+}
+var Q5='n&&no';
+var Q5=prompt("Is my favorite season is summer?" );
+if(Q5.toLowerCase()==='n'||Q5.toLowerCase==='no'){
+    alert('you are amzing');
+    // console.log('right')
+} else{
+    alert('actually it is not summer');
+}
+confirm('we are done ... Thanks or your time')
